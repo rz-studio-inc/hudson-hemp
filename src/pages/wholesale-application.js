12222895127816eddabf71098f0d1d6eb8bdaaba
rkartzman <remykartzman@gmail.com>
2019-08-06 18:37:18 -0400

    Update wholesale application

--- src/pages/wholesale-application.js
+++ src/pages/wholesale-application.js
@@ -2,9 +2,71 @@ import React from "react";
 import styled from 'styled-components';
 import Layout from "../layout/index";
 import {H1,H5} from '../common/Type';
+import media from '../common/mediaQuery';
 const Form = styled.form`
   display: flex;
   flex-direction: column;
+
+  .check-wrapper input[type="checkbox"] {
+      opacity: 0;
+  }
+  .check-wrapper label::before{
+    content: "";
+    display: inline-block;
+    
+    height: 16px;
+    width: 16px;
+    
+    border: 1px solid;   
+  }
+  .check-wrapper label::after {
+    content: "";
+    display: inline-block;
+    height: 6px;
+    width: 9px;
+    border-left: 2px solid;
+    border-bottom: 2px solid;
+    
+    transform: rotate(-45deg);
+  }
+  .check-wrapper label {
+    position: relative;
+    display: inline-block;
+  }
+  /* .check-wrapper label::before,
+  .check-wrapper label::after {
+      position: absolute;
+  } */
+  /*Outer-box*/
+  .check-wrapper label::before {
+    position: relative;
+    margin-right: 20px;
+    /* top: 3px; */
+  }
+  /*Checkmark*/
+  .check-wrapper label::after {
+    position: absolute;
+    width: 12px;
+    height: 12px;
+    top: calc(50% - 9px);
+    left: 2px;
+    opacity: 0;
+    background: #000;
+    border-radius: 50%;
+  }
+  /*Hide the checkmark by default*/
+  .check-wrapper input[type="checkbox"] + label::after {
+      /* content: none; */
+  }
+  /*Unhide the checkmark on the checked state*/
+  .check-wrapper input[type="checkbox"]:checked + label::after {
+      opacity: 1;
+      transition: opacity .4s ease-in-out;
+  }
+    /*Adding focus styles on the outer-box of the fake checkbox*/
+  .check-wrapper input[type="checkbox"]:focus + label::before {
+      /* outline: rgb(59, 153, 252) auto 5px; */
+  }
 
 `;
 const Submit = styled.input `
@@ -62,12 +124,16 @@ const Label = styled.label`
 
 
 const RailWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
+  display: block;
   top: 80px;
   position: relative;
   padding: 60px 40px 80px;
   background: #f8f5f5;
+  ${media.tablet`
+    display: flex;
+    flex-direction: row;
+
+  `}
 
 `;
 const Inner = styled.div`
@@ -80,16 +146,23 @@ const Inner = styled.div`
   }
 `;
 const LeftRail = styled.div`
-  max-width: 50%;
-  flex: 1 0 auto;
+  max-width: none;
+  ${media.tablet`
+    flex: 1 0 auto;
+    max-width: 50%;
+  `}
   
   
   
 `;
 const RightRail = styled.div`
-  max-width: 50%;
   
-  flex: 1 0 auto;
+  max-width: none;
+  ${media.tablet`
+    flex: 1 0 auto;
+    max-width: 50%;
+
+  `}
   em {
     font-size: 32px;
     line-height: 1.5;
@@ -109,6 +182,7 @@ const RightRail = styled.div`
 const Section = styled.div`
   display: flex;
   flex-direction: column;
+  max-width: 560px;
   &.row {
     flex-wrap: wrap;
     margin: 0 -10px;
@@ -134,6 +208,7 @@ const Dropdown = styled.select`
   -webkit-appearance: none;
   max-width: 560px;
   border: none;
+  border-radius: 0;
     background: #f8f5f5;
   -webkit-appearance: none;
   outline: none;
@@ -146,6 +221,8 @@ const Dropdown = styled.select`
   background-position: right 20px center;
   font-size: 21px;
   line-height: 1;
+  font-family: "Times New Roman",Times,serif;
+  margin-bottom: 12px;
 `;
 export default ({data}) => {
 
@@ -163,17 +240,76 @@ export default ({data}) => {
 
             <Input type="hidden" name="oid" value="00Df4000004jvuN" />
             <Input type="hidden" name="retURL" value="http://www.hudsonhemp.com/wholesale" />
-            <H5s>Company Information</H5s>
+            <H5s>1.Company Information</H5s>
             <Section>
 
               <label htmlFor="company">Company Name</label><Input  id="company" maxlength="40" name="company" size="20" type="text" />
 
               <label htmlFor="URL">Company Website</label><Input  id="URL" maxlength="80" name="URL" size="20" type="text" />
+              <label htmlFor="description">Description</label><Input name="description"></Input>
+              <label for="industry">Industry</label>
+              <Dropdown id="industry" name="industry">
+                <option value="">--None--</option>
+                <option value="Agriculture">Agriculture</option>
+                <option value="Apparel">Apparel</option>
+                <option value="Banking">Banking</option>
+                <option value="Biotechnology">Biotechnology</option>
+                <option value="Chemicals">Chemicals</option>
+                <option value="Consulting">Consulting</option>
+                <option value="Education">Education</option>
+                <option value="Electronics">Electronics</option>
+                <option value="Energy">Energy</option>
+                <option value="Engineering">Engineering</option>
+                <option value="Entertainment">Entertainment</option>
+                <option value="Environmental">Environmental</option>
+                <option value="Finance">Finance</option>
+                <option value="Food &amp; Beverage">Food &amp; Beverage</option>
+                <option value="Government">Government</option>
+                <option value="Healthcare">Healthcare</option>
+                <option value="Hospitality">Hospitality</option>
+                <option value="Insurance">Insurance</option>
+                <option value="Machinery">Machinery</option>
+                <option value="Manufacturing">Manufacturing</option>
+                <option value="Media">Media</option>
+                <option value="Not For Profit">Not For Profit</option>
+                <option value="Other">Other</option>
+                <option value="Recreation">Recreation</option>
+                <option value="Retail">Retail</option>
+                <option value="Shipping">Shipping</option>
+                <option value="Technology">Technology</option>
+                <option value="Telecommunications">Telecommunications</option>
+                <option value="Transportation">Transportation</option>
+                <option value="Utilities">Utilities</option>
+                <option value="Wellness">Wellness</option>
+                <option value="Beauty">Beauty</option>
+                <option value="Edibles">Edibles</option>
+                <option value="Home Goods">Home Goods</option>
+                <option value="Textiles">Textiles</option>
+                <option value="Paper">Paper</option>
+                <option value="Agriculture– Cannabis">Agriculture– Cannabis</option>
+              </Dropdown>
+              Government Document:
+              <Input id="00Nf400000UIuGW" maxlength="155" name="00Nf400000UIuGW" size="20" type="text" />
+              <label htmlFor="revenue">Annual Revenue</label>
+              <Input id="revenue" name="revenue" size="20" type="text" />
+              
+            </Section>
+            <H5s>2.Primary Contact</H5s>
+            <Section>
+              <InputWrapper>
+                <label htmlFor="first_name">First Name</label><Input id="first_name" maxlength="40" name="first_name" size="20" type="text" />
+              </InputWrapper>
+              <InputWrapper>
+                <label htmlFor="last_name">Last Name</label><Input id="last_name" maxlength="80" name="last_name" size="20" type="text" />
+              </InputWrapper>
 
-              <label htmlFor="phone">Phone</label><Input  id="phone" maxlength="40" name="phone" size="20" type="text" />
+              <label htmlFor="title">Title</label><Input id="title" maxlength="40" name="title" size="20" type="text" />
+
+              <label htmlFor="email">Email</label><Input id="email" maxlength="80" name="email" size="20" type="text" />
+              <label htmlFor="phone">Phone</label><Input id="phone" maxlength="40" name="phone" size="20" type="text" />
             </Section>
 
-            <H5s>Shipping Address</H5s>
+            <H5s>3.Shipping Information</H5s>
             <Section className="row">
               <InputWrapper className="input-half">
                 <label htmlFor="street">Street</label><Input name="street"></Input>
@@ -188,28 +324,8 @@ export default ({data}) => {
                 <label htmlFor="zip">Zip</label><Input  id="zip" maxlength="20" name="zip" size="20" type="text" />
               </InputWrapper>
             </Section>
-            <H5s>Primary Contact</H5s>
-            <Section>
-              <InputWrapper>
-                <label htmlFor="first_name">First Name</label><Input  id="first_name" maxlength="40" name="first_name" size="20" type="text" />
-              </InputWrapper>
-              <InputWrapper>
-                <label htmlFor="last_name">Last Name</label><Input  id="last_name" maxlength="80" name="last_name" size="20" type="text" />
-              </InputWrapper>
-            </Section>
-
-
-
-            <label htmlFor="title">Title</label><Input  id="title" maxlength="40" name="title" size="20" type="text" />
-
-            <label htmlFor="email">Email</label><Input  id="email" maxlength="80" name="email" size="20" type="text" />
-
-            <label htmlFor="description">Description</label><Input name="description"></Input>
-
-            Government Document:<Input  id="00Nf400000UIuGW" maxlength="155" name="00Nf400000UIuGW" size="20" type="text" />
-
-            <label htmlFor="revenue">Annual Revenue</label><Input  id="revenue" name="revenue" size="20" type="text" />
-            <H5s>Product Information</H5s>
+       
+            <H5s>4.Product Information</H5s>
             <Section>
 
               Which product are you interested in?:
@@ -220,13 +336,28 @@ export default ({data}) => {
                 <option value="Both">Both</option>
               </Dropdown>
 
-              The attributes of products:
-              <select  id="00Nf400000UIuGl" multiple="multiple" name="00Nf400000UIuGl" title="The attributes of products">
-                <option value="USDA Organic">USDA Organic</option>
-                <option value="Non-GMO">Non-GMO</option>
-                <option value="Vegan">Vegan</option>
-                <option value="Other">Other</option>
-              </select>
+                The attributes of products:
+
+                <div className="check-wrapper">
+                  <input id="00Nf400000UJY2e" name="00Nf400000UJY2e" type="checkbox" value="1" />
+                  <label htmlFor="00Nf400000UJY2e">USDA Organic:</label>
+                </div>
+                <div className="check-wrapper">
+                  <input id="00Nf400000UJY2j" name="00Nf400000UJY2j" type="checkbox" value="1" />
+                  <label htmlFor="00Nf400000UJY2j">Non-GMO:</label>
+                </div>
+                <div className="check-wrapper">
+                  <input id="00Nf400000UJY2o" name="00Nf400000UJY2o" type="checkbox" value="1" />
+                  <label htmlFor="00Nf400000UJY2o">Vegan:</label>
+                </div>
+                <div className="check-wrapper">
+                  <input id="00Nf400000UJY2t" name="00Nf400000UJY2t" type="checkbox" value="1" />
+                  <label htmlFor="00Nf400000UJY2t">Other:</label>
+                </div>
+              
+
+
+
             </Section>
             <H5s>Compliance Information</H5s>
             <Section>
@@ -239,7 +370,7 @@ export default ({data}) => {
 
               If yes, which lab?:<Input  id="00Nf400000UIuGv" maxlength="255" name="00Nf400000UIuGv" size="20" type="text" />
 
-              Is your product labeled and packaged for sale pursuant to FDA regulations for dietary supplemtns?:
+              Is your product labeled and packaged for sale pursuant to FDA regulations for dietary supplements?:
               <Dropdown  id="00Nf400000UIuHU" name="00Nf400000UIuHU" title="Does the product meet FDA regulations?">
                 <option value="">--None--</option>
                 <option value="Yes">Yes</option>
@@ -248,7 +379,7 @@ export default ({data}) => {
 
             </Section>
 
-            Source Comment:<Input  id="00Nf400000SmARU" name="00Nf400000SmARU" type="text" wrap="soft"></Input>
+            
 
             <Submit type="submit" name="submit" class="site-link" />
 
